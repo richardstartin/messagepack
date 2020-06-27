4cfbfd854e90e503470ee58c9bfb1c88786db957
richardstartin <richard@openkappa.co.uk>
2020-06-27 23:34:28 +0100

    get a tableswitch for int packing

--- src/main/java/io/github/richardstartin/messagepack/Packer.java
+++ src/main/java/io/github/richardstartin/messagepack/Packer.java
@@ -51,47 +51,6 @@ public class Packer {
 
     private final Codec codec;
 
-    private static final byte[] LZCNT_32 = new byte[] {
-            -1,
-            INT32,  INT32,  INT32,  INT32,  INT32,  INT32,  INT32, INT32,
-            INT32,  INT32,  INT32,  INT32,  INT32,  INT32,  INT32, INT16,
-            INT16,  INT16,  INT16,  INT16,  INT16,  INT16,  INT16,  INT8,
-            FIXNUM,  FIXNUM, FIXNUM, FIXNUM, FIXNUM, FIXNUM, FIXNUM, FIXNUM
-    };
-
-    private static final byte[] LZCNT_32_INV = new byte[] {
-            -1,
-            INT32,  INT32,  INT32,  INT32,  INT32,  INT32,  INT32, INT32,
-            INT32,  INT32,  INT32,  INT32,  INT32,  INT32,  INT32, INT32,
-            INT16,  INT16,  INT16,  INT16,  INT16,  INT16,  INT16, INT16,
-            INT8,   INT8, FIXNUM, FIXNUM, FIXNUM, FIXNUM, FIXNUM, FIXNUM
-    };
-
-    private static final byte[] LZCNT_64 = new byte[] {
-            -1,
-            INT64, INT64, INT64, INT64, INT64, INT64, INT64, INT64,
-            INT64, INT64, INT64, INT64, INT64, INT64, INT64, INT64,
-            INT64, INT64, INT64, INT64, INT64, INT64, INT64, INT64,
-            INT64, INT64, INT64, INT64, INT64, INT64, INT64, INT32,
-            INT32,  INT32,  INT32,  INT32,  INT32,  INT32,  INT32, INT32,
-            INT32,  INT32,  INT32,  INT32,  INT32,  INT32,  INT32, INT16,
-            INT16,  INT16,  INT16,  INT16,  INT16,  INT16,  INT16,  INT8,
-            FIXNUM,  FIXNUM, FIXNUM, FIXNUM, FIXNUM, FIXNUM, FIXNUM, FIXNUM
-    };
-
-    private static final byte[] LZCNT_64_INV = new byte[] {
-            -1,
-            INT64, INT64, INT64, INT64, INT64, INT64, INT64, INT64,
-            INT64, INT64, INT64, INT64, INT64, INT64, INT64, INT64,
-            INT64, INT64, INT64, INT64, INT64, INT64, INT64, INT64,
-            INT64, INT64, INT64, INT64, INT64, INT64, INT64, INT64,
-            INT32,  INT32,  INT32,  INT32,  INT32,  INT32,  INT32, INT32,
-            INT32,  INT32,  INT32,  INT32,  INT32,  INT32,  INT32, INT32,
-            INT16,  INT16,  INT16,  INT16,  INT16,  INT16,  INT16, INT16,
-            INT8,   INT8, FIXNUM, FIXNUM, FIXNUM, FIXNUM, FIXNUM, FIXNUM
-    };
-
-
     private final Consumer<ByteBuffer> blockingSink;
     private final ByteBuffer buffer;
     private int messageCount = 0;
@@ -229,86 +188,265 @@ public class Packer {
 
     public void writeInt(int value) {
         if (value < 0) {
-            switch (LZCNT_32_INV[Integer.numberOfLeadingZeros(~value)]) {
-                case FIXNUM:
-                    buffer.put((byte)(NEGFIXNUM | value));
+            switch (Integer.numberOfLeadingZeros(~value)) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                    buffer.put(INT32);
+                    buffer.putInt(value);
                     break;
-                case INT8:
-                    buffer.put(INT8);
-                    buffer.put((byte)value);
-                    break;
-                case INT16:
+                case 17:
+                case 18:
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                case 24:
                     buffer.put(INT16);
                     buffer.putChar((char)value);
                     break;
-                case INT32:
+                case 25:
+                case 26:
+                    buffer.put(INT8);
+                    buffer.put((byte)value);
+                    break;
+                case 27:
+                case 28:
+                case 29:
+                case 30:
+                case 31:
+                case 32:
                 default:
-                    buffer.put(INT32);
-                    buffer.putInt(value);
+                    buffer.put((byte)(NEGFIXNUM | value));
             }
         } else {
-            switch (LZCNT_32[Integer.numberOfLeadingZeros(value)]) {
-                case FIXNUM:
-                    buffer.put((byte)value);
+            switch (Integer.numberOfLeadingZeros(value)) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                    buffer.put(INT32);
+                    buffer.putInt(value);
                     break;
-                case INT8:
-                    buffer.put(INT8);
-                    buffer.put((byte)value);
-                    break;
-                case INT16:
+                case 16:
+                case 17:
+                case 18:
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
                     buffer.put(INT16);
                     buffer.putChar((char)value);
                     break;
-                case INT32:
+                case 24:
+                    buffer.put(INT8);
+                    buffer.put((byte)value);
+                    break;
+                case 25:
+                case 26:
+                case 27:
+                case 28:
+                case 29:
+                case 30:
+                case 31:
+                case 32:
                 default:
-                    buffer.put(INT32);
-                    buffer.putInt(value);
+                    buffer.put((byte)value);
             }
         }
     }
 
     public void writeLong(long value) {
         if (value < 0) {
-            switch (LZCNT_64_INV[Long.numberOfLeadingZeros(~value)]) {
-                case FIXNUM:
-                    buffer.put((byte)(NEGFIXNUM | value));
+            switch (Long.numberOfLeadingZeros(~value)) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                case 24:
+                case 25:
+                case 26:
+                case 27:
+                case 28:
+                case 29:
+                case 30:
+                case 31:
+                case 32:
+                    buffer.put(INT64);
+                    buffer.putLong(value);
                     break;
-                case INT8:
-                    buffer.put(INT8);
-                    buffer.put((byte)value);
+                case 33:
+                case 34:
+                case 35:
+                case 36:
+                case 37:
+                case 38:
+                case 39:
+                case 40:
+                case 41:
+                case 42:
+                case 43:
+                case 44:
+                case 45:
+                case 46:
+                case 47:
+                case 48:
+                    buffer.put(INT32);
+                    buffer.putInt((int)value);
                     break;
-                case INT16:
+                case 49:
+                case 50:
+                case 51:
+                case 52:
+                case 53:
+                case 54:
+                case 55:
+                case 56:
                     buffer.put(INT16);
                     buffer.putChar((char)value);
                     break;
-                case INT32:
-                    buffer.put(INT32);
-                    buffer.putInt((int)value);
-                case INT64:
+                case 57:
+                case 58:
+                    buffer.put(INT8);
+                    buffer.put((byte)value);
+                    break;
+                case 59:
+                case 60:
+                case 61:
+                case 62:
+                case 63:
+                case 64:
                 default:
-                    buffer.put(INT64);
-                    buffer.putLong(value);
+                    buffer.put((byte)(NEGFIXNUM | value));
             }
         } else {
-            switch (LZCNT_64[Long.numberOfLeadingZeros(value)]) {
-                case FIXNUM:
-                    buffer.put((byte)value);
+            switch (Long.numberOfLeadingZeros(value)) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                case 24:
+                case 25:
+                case 26:
+                case 27:
+                case 28:
+                case 29:
+                case 30:
+                case 31:
+                    buffer.put(INT64);
+                    buffer.putLong(value);
                     break;
-                case INT8:
-                    buffer.put(INT8);
-                    buffer.put((byte)value);
+                case 32:
+                case 33:
+                case 34:
+                case 35:
+                case 36:
+                case 37:
+                case 38:
+                case 39:
+                case 40:
+                case 41:
+                case 42:
+                case 43:
+                case 44:
+                case 45:
+                case 46:
+                case 47:
+                    buffer.put(INT32);
+                    buffer.putInt((int)value);
                     break;
-                case INT16:
+                case 48:
+                case 49:
+                case 50:
+                case 51:
+                case 52:
+                case 53:
+                case 54:
+                case 55:
                     buffer.put(INT16);
                     buffer.putChar((char)value);
                     break;
-                case INT32:
-                    buffer.put(INT32);
-                    buffer.putInt((int)value);
-                case INT64:
+                case 56:
+                    buffer.put(INT8);
+                    buffer.put((byte)value);
+                    break;
+                case 57:
+                case 59:
+                case 60:
+                case 61:
+                case 62:
+                case 63:
+                case 64:
                 default:
-                    buffer.put(INT64);
-                    buffer.putLong(value);
+                    buffer.put((byte)value);
             }
         }
     }
